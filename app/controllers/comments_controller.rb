5e515da48003a71b878bba8c49a4e0d2c17ff823
Kamil Pek <kamil.pek@gmail.com>
2017-02-16 17:09:45 +0100

    spolszczenia

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -40,7 +40,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to @comment, notice: 'Dodano komentarz.' }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
@@ -54,7 +54,7 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.html { redirect_to @comment, notice: 'Zmodyfikowano komentarz.' }
         format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit }
@@ -68,7 +68,7 @@ class CommentsController < ApplicationController
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to comments_url, notice: 'Usunięto komentarz.' }
       format.json { head :no_content }
     end
   end
