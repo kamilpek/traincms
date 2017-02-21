f5749c77c2a805bf78628548b9e6dd4a2e4bce9b
Kamil Pek <kamil.pek@gmail.com>
2017-02-21 21:02:56 +0100

    kalendarz - poczatki

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resource :calendar, only: [:show], controller: :calendar
+
   mount Ckeditor::Engine => '/ckeditor'
   resources :comments
   resources :components
