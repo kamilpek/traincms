b4a18ab6c5e3fae2790bd55ba1088f213cf60c04
Kamil Pek <kamil.pek@gmail.com>
2017-04-07 16:53:17 +0200

    cloudinary poprawki

--- app/uploaders/ckeditor_attachment_file_uploader.rb
+++ app/uploaders/ckeditor_attachment_file_uploader.rb
@@ -11,15 +11,15 @@ class CkeditorAttachmentFileUploader < CarrierWave::Uploader::Base
   # include CarrierWave::ImageScience
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
 
   process :tags => ['ckeditor_attachment']
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/ckeditor/attachments/#{model.id}"
-  end
+  # def store_dir
+  #   "uploads/ckeditor/attachments/#{model.id}"
+  # end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
