be76675282b645ac5898d04ccb3d7b6167093009
Kamil Pek <kamil.pek@gmail.com>
2017-04-07 18:37:50 +0200

    wyszukiwarka, wyswietlanie artykułów, users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def home
     @categories = Category.where("home_on = ?", true)
     @articles = Article.where(category_id:@categories)
+    @articles = @articles.where(active:1)
     @articles = @articles.paginate(:page => params[:page], :per_page => 5)
     @tags = Tag.where("active = ?", true)
     @events = Event.where("active = ?", true)
@@ -16,6 +17,7 @@ class PagesController < ApplicationController
   def search
     @categories = Category.where("home_on = ?", true)
     @articles = Article.where(category_id:@categories)
+    @articles = @articles.where(active:1)
     if params[:search].nil?
       @articles = @articles.where(id:0)
     else
